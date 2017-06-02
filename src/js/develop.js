fd3ec5973473fa70dec97359088d2e1f307eb4b6
unknown <trolizmaslom@gmail.com>
2017-06-02 17:22:22 +0300

    rjvbn

--- src/js/develop.js
+++ src/js/develop.js
@@ -167,17 +167,23 @@ function filterCheckboxLogic(){
     }
 }
 function rangeLogic() {
-    $( "#slider-range" ).slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [ 75, 300 ],
-        slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-        }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    var range = $( "#slider-range" );
+    if(range.length > 0){
+        range.slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [ 0, 250 ],
+            classes: {"ui-slider-handle": "leftright"},
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            }
+        });
+        $( "#amount" ).val( "$" + range.slider( "values", 0 ) +
+            " - $" + range.slider( "values", 1 ) );
+    }
+
+
 };
 $(document).ready(function () {
     dropdowns();
