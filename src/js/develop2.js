2227ca9ff1e6fb207fcd362c80347e34b092694c
Romanyuk <hotpick18@gmail.com>
2017-07-04 14:27:52 +0300

    fix

--- src/js/develop2.js
+++ src/js/develop2.js
@@ -240,9 +240,7 @@ var PopupMap = (function(){
 
 })();
 
-
-
- window.Gmap = null;
+window.Gmap = null;
 
  var GMapHandler = function() {
 	 var e = $(".contacts__map-wrap");
@@ -353,117 +351,4 @@ var PopupMap = (function(){
 		 });
 		 l.setMap(window.Gmap);
 	 };
- };
-
- window.Gmap = null;
-
- var GMapHandler = function() {
-		 var e = $(".contacts__map-wrap");
-		 var i = "55.715797, 37.681662";
-		 var t = "55.715797, 37.681662";
-		 var a = "images/marker.png";
-		 var n = 16;
-		 if (!e.length) {
-				 return;
-		 }
-		 if (!window.map_coords) {
-				 console.log("[Gmap] Не заданы координаты в параметре map_coord. Используем стандартное значение");
-				 window.map_coords = "55.69777704873052,37.77824859751695;55.7535378290641,37.624929644118836;10";
-		 }
-		 var r = window.map_coords.split(";");
-		 if (r[0]) i = r[0];
-		 if (r[1]) t = r[1];
-		 if (r[2]) n = r[2];
-		 i = i.split(",");
-		 t = t.split(",");
-		 n = parseInt(n);
-		 var l = $(window).height();
-		 var o = false;
-		 var d = e.offset().top;
-		 setInterval(function() {
-				 if (!o) d = e.offset().top;
-		 }, 2e3);
-		 setInterval(function() {
-				 var e = $(document).scrollTop();
-				 if (!o && e > d - l * 3) {
-						 $.getScript("http://maps.googleapis.com/maps/api/js?v=3.9&sensor=false&callback=gMapInitialize&key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
-						 o = true;
-				 }
-		 }, 20);
-		 window.gMapInitialize = function() {
-				 window.Gmap = new google.maps.Map(document.getElementById("map"), {
-						 zoom: n,
-						 center: new google.maps.LatLng(parseFloat(i[0]), parseFloat(i[1])),
-						 mapTypeControlOptions: {
-								 mapTypeIds: [ google.maps.MapTypeId.ROADMAP, "tehgrayz" ]
-						 },
-						 scrollwheel: false
-				 });
-				 var e = new google.maps.StyledMapType([ {
-						 featureType: "all",
-						 stylers: [ {
-								 hue: "#0000ff"
-						 }, {
-								 saturation: -95
-						 } ]
-				 }, {
-						 featureType: "poi",
-						 elementType: "label",
-						 stylers: [ {
-								 visibility: "off"
-						 } ]
-				 } ], {
-						 name: "Grayscale"
-				 });
-				 window.Gmap.mapTypes.set("tehgrayz", e);
-				 window.Gmap.setMapTypeId("tehgrayz");
-				 new google.maps.Marker({
-						 position: new google.maps.LatLng(parseFloat(t[0]), parseFloat(t[1])),
-						 map: window.Gmap,
-						 clickable: false,
-						 icon: {
-								 url: a,
-								 origin: new google.maps.Point(0, 0)
-						 }
-				 });
-				 new google.maps.Marker({
-						 position: new google.maps.LatLng(parseFloat(55.7156), parseFloat(37.68178)),
-						 map: window.Gmap,
-						 clickable: false,
-						 icon: {
-								 url: "images/map-arrow.png",
-								 origin: new google.maps.Point(0, 0)
-						 }
-				 });
-				 new google.maps.Marker({
-						 position: new google.maps.LatLng(parseFloat(55.717678), parseFloat(37.675938)),
-						 map: window.Gmap,
-						 clickable: false,
-						 icon: {
-								 url: "images/metro.png",
-								 origin: new google.maps.Point(0, 0)
-						 }
-				 });
-				 var r = [ {
-						 lat: 55.717894,
-						 lng: 37.676866
-				 }, {
-						 lat: 55.714848,
-						 lng: 37.679173
-				 }, {
-						 lat: 55.715585,
-						 lng: 37.681705
-				 }, {
-						 lat: 55.7156,
-						 lng: 37.681782
-				 } ];
-				 var l = new google.maps.Polyline({
-						 path: r,
-						 geodesic: true,
-						 strokeColor: "#f4b816",
-						 strokeOpacity: 1,
-						 strokeWeight: 5
-				 });
-				 l.setMap(window.Gmap);
-		 };
  };
