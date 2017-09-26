7c1d932b1a68b42e3fe2a06d8c6d5a44b1c9b984
Romanyuk <hotpick18@gmail.com>
2017-09-26 12:18:06 +0300

    actual updates

--- src/js/develop.js
+++ src/js/develop.js
@@ -156,8 +156,8 @@ function selectStyler(){
     var select2 = $('.feed-form__select select');
     if(select2.length > 0){
         select2.styler({
-
-            selectSmartPositioning:false
+            selectSmartPositioning:false,
+            selectSearchLimit: 4
         });
     }
     var select3 = $('.card__select select');
@@ -795,3 +795,265 @@ $(document).ready(function () {
     $('.slider1').slick();
 
 });
+
+
+/*dev2*/
+
+var addDelMap = (function(){
+    var btn = $('.js-del-map');
+
+    btn.click(function(e){
+        e.preventDefault();
+        $(this).next().slideToggle();
+        var $map = $('#map');
+        if(addMap){
+            $.getScript("http://maps.googleapis.com/maps/api/js?v=3.9&sensor=false&callback=gMapInitialize&key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
+            addMap = !addMap;
+        }
+    });
+})();
+
+var addMap = true; 
+var PopupMap = (function(){
+    var $btn = $('.js-btn-map');
+    var $mapClose = $('.js-map-close');
+
+    $("[data-fancybox-map]").fancybox({
+        modal: true,
+        touch : {
+            vertical : false,  // Allow to drag content vertically
+            momentum : false   // Continue movement after releasing mouse/touch when panning
+        },
+        afterLoad: function() {
+            setTimeout(function() {
+                initGMap();
+            }, 500);
+            if (addMap){
+                console.log('add map')
+                $.getScript("http://maps.googleapis.com/maps/api/js?v=3.9key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
+                addMap = !addMap;
+            }
+        }
+    });
+
+    $mapClose.click(function(){
+        $.fancybox.close();
+    });
+
+    function initGMap() {
+        var map;
+        var zoom;
+
+        map = new google.maps.Map(document.getElementById('popmap'), {
+            center: {lat: 55.755231, lng: 37.616549}, 
+            zoom: 12,
+            mapTypeControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        infoWindow = new google.maps.InfoWindow({
+            maxWidth: 240
+        });
+
+        google.maps.event.addListener(map, "click", function() {
+            // infoWindow.close - закрываем информационное окно.
+            infoWindow.close();
+        });
+
+        var markersData = [
+            {
+                lat: 55.764246,
+                lng: 37.618367,
+                address: "ХОРОШЕВСКОЕ Ш. 41А",
+                phone: "+7 (499) 677-20-69",
+                workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
+            },
+            {
+                lat: 55.757881,
+                lng: 37.610806,
+                address: "ХОРОШЕВСКОЕ Ш. 42А",
+                phone: "+7 (499) 677-20-69",
+                workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
+            },
+            {
+                lat: 55.762683,
+                lng: 37.660390,
+                address: "ХОРОШЕВСКОЕ Ш. 43А",
+                phone: "+7 (499) 677-20-69",
+                workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
+            },
+            {
+                lat: 55.762683,
+                lng: 37.630843,
+                address: "ХОРОШЕВСКОЕ Ш. 44А",
+                phone: "+7 (499) 677-20-69",
+                workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
+            },
+            {
+                lat: 55.738175,
+                lng: 37.589916,
+                address: "ХОРОШЕВСКОЕ Ш. 45А",
+                phone: "+7 (499) 677-20-69",
+                workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
+            }
+        ];
+
+        for (var i = 0; i < markersData.length; i++){
+            var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+            var address = markersData[i].address;
+            var phone = markersData[i].phone;
+            var workhours = markersData[i].workhours;
+            // Добавляем маркер с информационным окном
+            addMarker(latLng, address , workhours, phone);
+        }
+
+        function addMarker(latLng, address , workhours, phone) {
+                var marker = new google.maps.Marker({
+                        position: latLng,
+                        map: map,
+                        address: address,
+                        icon: {
+                            url: "images/marker1.png"
+                        }
+                });
+            
+                // Отслеживаем клик по нашему маркеру
+                google.maps.event.addListener(marker, "click", function() {
+                    // contentString - это переменная в которой хранится содержимое информационного окна.
+                    var contentString = '<div class="map-modal__info-block">' +
+                        '<div class="map-modal__address">' + address + '</div>' +
+                        '<div class="map-modal__phone">' + phone + '</div>' +
+                        '<div class="map-modal__workhours">' + workhours + '</div>' +
+                        '<a href="#" class="button button__yellow map-modal__button"><span>Выбрать</span></a>'
+                        '</div>';
+                    // Меняем содержимое информационного окна
+                    infoWindow.setContent(contentString);
+                    // Показываем информационное окно
+                    infoWindow.open(map, marker);
+            
+                });
+        }
+
+        $btn.click(function(){
+            zoom = map.getZoom();
+            if (zoom > 9) map.setZoom(16);
+            var lat = $(this).data('lat');
+            var lng = $(this).data('lng');
+            map.setCenter(new google.maps.LatLng(lat, lng));
+        });
+    }
+})();
+
+window.Gmap = null;
+
+ var GMapHandler = function() {
+     var e = $(".contacts__map-wrap");
+     var i = "55.715797, 37.681662";
+     var t = "55.715797, 37.681662";
+     var a = "images/marker.png";
+     var n = 16;
+     if (!e.length) {
+         return;
+     }
+     if (!window.map_coords) {
+         /*console.log("[Gmap] Не заданы координаты в параметре map_coord. Используем стандартное значение");*/
+         window.map_coords = "55.69777704873052,37.77824859751695;55.7535378290641,37.624929644118836;10";
+     }
+     var r = window.map_coords.split(";");
+     if (r[0]) i = r[0];
+     if (r[1]) t = r[1];
+     if (r[2]) n = r[2];
+     i = i.split(",");
+     t = t.split(",");
+     n = parseInt(n);
+     var l = $(window).height();
+     var o = false;
+     var d = e.offset().top;
+     setInterval(function() {
+         if (!o) d = e.offset().top;
+     }, 2e3);
+     setInterval(function() {
+         var e = $(document).scrollTop();
+         if (!o && e > d - l * 3) {
+             $.getScript("http://maps.googleapis.com/maps/api/js?v=3.9&sensor=false&callback=gMapInitialize&key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
+             o = true;
+         }
+     }, 20);
+
+     window.gMapInitialize = function() {
+         window.Gmap = new google.maps.Map(document.getElementById("map"), {
+             zoom: n,
+             center: new google.maps.LatLng(parseFloat(i[0]), parseFloat(i[1])),
+             mapTypeControlOptions: {
+                 mapTypeIds: [ google.maps.MapTypeId.ROADMAP, "tehgrayz" ]
+             },
+             scrollwheel: false
+         });
+         var e = new google.maps.StyledMapType([ {
+             featureType: "all",
+             stylers: [ {
+                 hue: "#0000ff"
+             }, {
+                 saturation: -95
+             } ]
+         }, {
+             featureType: "poi",
+             elementType: "label",
+             stylers: [ {
+                 visibility: "off"
+             } ]
+         } ], {
+             name: "Grayscale"
+         });
+         window.Gmap.mapTypes.set("tehgrayz", e);
+         window.Gmap.setMapTypeId("tehgrayz");
+         new google.maps.Marker({
+             position: new google.maps.LatLng(parseFloat(t[0]), parseFloat(t[1])),
+             map: window.Gmap,
+             clickable: false,
+             icon: {
+                 url: a,
+                 origin: new google.maps.Point(0, 0)
+             }
+         });
+         new google.maps.Marker({
+             position: new google.maps.LatLng(parseFloat(55.7156), parseFloat(37.68178)),
+             map: window.Gmap,
+             clickable: false,
+             icon: {
+                 url: "images/map-arrow.png",
+                 origin: new google.maps.Point(0, 0)
+             }
+         });
+         new google.maps.Marker({
+             position: new google.maps.LatLng(parseFloat(55.717678), parseFloat(37.675938)),
+             map: window.Gmap,
+             clickable: false,
+             icon: {
+                 url: "images/metro.png",
+                 origin: new google.maps.Point(0, 0)
+             }
+         });
+         var r = [ {
+             lat: 55.717894,
+             lng: 37.676866
+         }, {
+             lat: 55.714848,
+             lng: 37.679173
+         }, {
+             lat: 55.715585,
+             lng: 37.681705
+         }, {
+             lat: 55.7156,
+             lng: 37.681782
+         } ];
+         var l = new google.maps.Polyline({
+             path: r,
+             geodesic: true,
+             strokeColor: "#f4b816",
+             strokeOpacity: 1,
+             strokeWeight: 5
+         });
+         l.setMap(window.Gmap);
+     };
+ };
--- src/js/develop2.js
+++ src/js/develop2.js
@@ -10,9 +10,8 @@
 			watch		: true,
 			height		: null,
 			tolerance	: 0
-		});
+	});
 })();
-
 $(document).ready(function() {
 		GMapHandler();
 		$('.js-sales-slider').slick({
@@ -71,21 +70,6 @@ $(document).ready(function() {
 				]
 		});
 });
-
-var addDelMap = (function(){
-	var btn = $('.js-del-map');
-
-	btn.click(function(e){
-		e.preventDefault();
-		$(this).next().slideToggle();
-		var $map = $('#map');
-		if(addMap){
-			$.getScript("http://maps.googleapis.com/maps/api/js?v=3.9&sensor=false&callback=gMapInitialize&key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
-			addMap = !addMap;
-		}
-	});
-})();
-
 var addDelList = (function(){
 	var btn = $('.js-del-list');
 
@@ -94,32 +78,26 @@ var addDelList = (function(){
 		$(this).next().slideToggle();
 	});
 })();
-
 var filterAlph = (function(){
 	var $letter = $('.js-letter');
 	var $input = $('.js-alph-input');
 	var $close = $('.js-alph-close');
 	var $reset = $('.js-alph-reset');
-
 	$letter.on('click', function(){
 		$(this).parent().toggleClass('active');
 	});
-
 	$close.on('click', function(){
 		if($(this).parent().find($input).not(":checked")){
 			$(this).parent().parent().hide();
 		}
 	});
-
 	$reset.on('click', function(e){
 		e.preventDefault();
 		$('.alph-form__item').show();
 		$('.alph-form__item').removeClass('active');
 		$input.prop('checked',false);
 	});
-
 })();
-
 var filterDrop = (function(){
 	var $btn = $('.js-sw-trg');
 	var $reset = $('.js-check-reset');
@@ -135,18 +113,24 @@ var filterDrop = (function(){
 		$(this).parent().next().slideToggle();
 	});
 })();
-
 var sendFile = (function(){
-		$('.js-send-file').change(function() {
-				var filename = $(this).val().split('\\').pop();;
+		/*$('.js-send-file').change(function() {
+				var filename = $(this).val().split('\\').pop();
 				var filePath = $(this).val();
 				$('.file-wrap').append('<div class="file-field__text-wrap"><div class="file-field__text file-field__text--done">' + filename + '</div><input type="file" class="file-field__input" name="user_file[]" value=' + filePath + ' /> </div>');
 				$(this).val('');
+		});*/
+		$('.js-send-file').change(function() {
+				var names = [];
+				for (var i = 0; i < $(this).get(0).files.length; ++i) {
+					names.push($(this).get(0).files[i].name);
+				}
+				console.log($(this));
+				$(this).parent().after('<div class="file-wrap"></div>');
+				$(this).parent().next('.file-wrap').html(names.join('<br>'));
 		});
 })();
-
 var ProductSheet = (function(){
-
 	$('.js-sheet-slider').slick({
 			slidesToShow: 1,
 			arrows:true
@@ -178,9 +162,7 @@ var ProductSheet = (function(){
 			$('.js-prodsheet-slider').slick('unslick');
 		}
 	});
-
 })();
-
 var videoPop = (function(){
 		var $targetBtn = $('.js-target-btn');
 		var $tab = $('.js-tab');
@@ -212,7 +194,6 @@ var videoPop = (function(){
 		});
 
 })();
-
 var tabsLk = (function(){
 	var tab = $('.js-tab-lk');
 
@@ -221,9 +202,7 @@ var tabsLk = (function(){
 		tab.removeClass('active');
 		$(this).addClass('active');
 	});
-
 })();
-
 var showMoreItem = (function(){
 		var $btn = $('.js-show-link');
 		var $item = $('.prod-card__list .hidden');
@@ -241,249 +220,145 @@ var showMoreItem = (function(){
 						}
 				});
 		}
+})();
+var modalM = (function(){
+		var input = $('.js-dropm');
+		var hide = $(this).parent().parent().next();
+
+		input.on('change', function(){
+				$('.cancel-modal__hidden').removeClass('active');
+				$('.cancel-modal__hidden').slideUp();
+				if($('.js-dropm:checked').hasClass('js-act')){
+								$('.js-act:checked').parent().parent().next('.cancel-modal__hidden').addClass('active');
+								$('.js-act:checked').parent().parent().next('.cancel-modal__hidden').slideDown();;
+				}else{
+								$('.cancel-modal__hidden').removeClass('active');
+								$('.cancel-modal__hidden').slideUp();
+				}
+		});
+})();
+
+var modalPhone = (function(){
+		var btn = $('.js-show-number');
+
+		btn.click(function(e){
+				e.preventDefault();
+				$(this).next().show();
+				$(this).hide();
+		});
 
 })();
-var addMap = true; 
-var PopupMap = (function(){
-	var $btn = $('.js-btn-map');
-	var $mapClose = $('.js-map-close');
 
-	$("[data-fancybox-map]").fancybox({
-		modal: true,
-		touch : {
-			vertical : false,  // Allow to drag content vertically
-			momentum : false   // Continue movement after releasing mouse/touch when panning
-		},
-		afterLoad: function() {
-			setTimeout(function() {
-				initGMap();
-			}, 500);
-			if (addMap){
-				console.log('add map')
-				$.getScript("http://maps.googleapis.com/maps/api/js?v=3.9key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
-				addMap = !addMap;	
-			}
-		}
-	});
+var search = (function(){
+		var searchM = $('.js-search-m');
+		var close = $('.js-close');
 
-	$mapClose.click(function(){
-		$.fancybox.close();
-	});
-
-	function initGMap() {
-		var map;
-		var zoom;
-
-		map = new google.maps.Map(document.getElementById('popmap'), {
-			center: {lat: 55.755231, lng: 37.616549}, 
-			zoom: 12,
-			mapTypeControl: false,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
+		searchM.keyup(function(){
+				if($(this).val().length > 0){
+						close.show();
+				} else if($(this).val().length === 0){
+						close.hide();
+				}
+				close.click(function(){
+						$(this).hide();
+				});
 		});
 
-		infoWindow = new google.maps.InfoWindow({
-			maxWidth: 240
+		$(window).scroll(function(){
+				var topLine = $('.js-search');
+				if($(this).scrollTop() >= 170 ){
+						topLine.addClass("active");
+				} else if($(this).scrollTop() <= 170 ){
+						topLine.removeClass("active");
+				}
 		});
 
-		google.maps.event.addListener(map, "click", function() {
-			// infoWindow.close - закрываем информационное окно.
-			infoWindow.close();
-		});
+		if($(window).width() < 500){
+						$('.s-blog__text p').addClass('js-cut-text');
+				}else if($(window).width() > 500)
+				{
+						$('.s-blog__text p').removeClass('js-cut-text');
+				}
 
-		var markersData = [
-			{
-				lat: 55.764246,
-				lng: 37.618367,
-				address: "ХОРОШЕВСКОЕ Ш. 41А",
-				phone: "+7 (499) 677-20-69",
-				workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
-			},
-			{
-				lat: 55.757881,
-				lng: 37.610806,
-				address: "ХОРОШЕВСКОЕ Ш. 42А",
-				phone: "+7 (499) 677-20-69",
-				workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
-			},
-			{
-				lat: 55.762683,
-				lng: 37.660390,
-				address: "ХОРОШЕВСКОЕ Ш. 43А",
-				phone: "+7 (499) 677-20-69",
-				workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
-			},
-			{
-				lat: 55.762683,
-				lng: 37.630843,
-				address: "ХОРОШЕВСКОЕ Ш. 44А",
-				phone: "+7 (499) 677-20-69",
-				workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
-			},
-			{
-				lat: 55.738175,
-				lng: 37.589916,
-				address: "ХОРОШЕВСКОЕ Ш. 45А",
-				phone: "+7 (499) 677-20-69",
-				workhours: "пн-пт 10:00-20:00, сб 10:00-16:00, вс 10:00-14:00"
-			}
-		];
-
-		for (var i = 0; i < markersData.length; i++){
-			var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
-			var address = markersData[i].address;
-			var phone = markersData[i].phone;
-			var workhours = markersData[i].workhours;
-			// Добавляем маркер с информационным окном
-			addMarker(latLng, address , workhours, phone);
-		}
-
-		function addMarker(latLng, address , workhours, phone) {
-				var marker = new google.maps.Marker({
-						position: latLng,
-						map: map,
-						address: address,
-						icon: {
-							url: "images/marker1.png"
+		$(window).resize(function(){
+				if($(window).width() < 500)
+						{
+								$('.s-blog__text p').addClass('js-cut-text');
+						}else if($(window).width() > 500)
+						{
+								$('.s-blog__text p').removeClass('js-cut-text');
 						}
-				});
-			
-				// Отслеживаем клик по нашему маркеру
-				google.maps.event.addListener(marker, "click", function() {
-					// contentString - это переменная в которой хранится содержимое информационного окна.
-					var contentString = '<div class="map-modal__info-block">' +
-						'<div class="map-modal__address">' + address + '</div>' +
-						'<div class="map-modal__phone">' + phone + '</div>' +
-						'<div class="map-modal__workhours">' + workhours + '</div>' +
-						'<a href="#" class="button button__yellow map-modal__button"><span>Выбрать</span></a>'
-						'</div>';
-					// Меняем содержимое информационного окна
-					infoWindow.setContent(contentString);
-					// Показываем информационное окно
-					infoWindow.open(map, marker);
-			
-				});
-		}
-
-		$btn.click(function(){
-			zoom = map.getZoom();
-			if (zoom > 9) map.setZoom(16);
-			var lat = $(this).data('lat');
-			var lng = $(this).data('lng');
-			map.setCenter(new google.maps.LatLng(lat, lng));
 		});
-	}
 })();
 
-window.Gmap = null;
+var editBlock = (function(){
+	var block = $('.js-edit-block');
+	var callBtn = $('.js-edit');
+	var closeBtn = $('.js-closed');
 
- var GMapHandler = function() {
-	 var e = $(".contacts__map-wrap");
-	 var i = "55.715797, 37.681662";
-	 var t = "55.715797, 37.681662";
-	 var a = "images/marker.png";
-	 var n = 16;
-	 if (!e.length) {
-		 return;
-	 }
-	 if (!window.map_coords) {
-		 /*console.log("[Gmap] Не заданы координаты в параметре map_coord. Используем стандартное значение");*/
-		 window.map_coords = "55.69777704873052,37.77824859751695;55.7535378290641,37.624929644118836;10";
-	 }
-	 var r = window.map_coords.split(";");
-	 if (r[0]) i = r[0];
-	 if (r[1]) t = r[1];
-	 if (r[2]) n = r[2];
-	 i = i.split(",");
-	 t = t.split(",");
-	 n = parseInt(n);
-	 var l = $(window).height();
-	 var o = false;
-	 var d = e.offset().top;
-	 setInterval(function() {
-		 if (!o) d = e.offset().top;
-	 }, 2e3);
-	 setInterval(function() {
-		 var e = $(document).scrollTop();
-		 if (!o && e > d - l * 3) {
-			 $.getScript("http://maps.googleapis.com/maps/api/js?v=3.9&sensor=false&callback=gMapInitialize&key=AIzaSyC0_lMgx4X2bzB2ebVtlpcZIF6VAhK0NgE");
-			 o = true;
-		 }
-	 }, 20);
+	callBtn.click(function(e){
+		e.preventDefault();
+		$( block ).clone().prependTo($(this).closest('.feedbacks__item'));
+		$(this).closest('.feedbacks__m-cont').stop().slideUp();
+		$(this).closest('.dropdown-container').find('.dropdown-btn').removeClass('active');
+		$(this).closest('.dropdown-container').addClass('hide-drop').hide();
+	});
 
-	 window.gMapInitialize = function() {
-		 window.Gmap = new google.maps.Map(document.getElementById("map"), {
-			 zoom: n,
-			 center: new google.maps.LatLng(parseFloat(i[0]), parseFloat(i[1])),
-			 mapTypeControlOptions: {
-				 mapTypeIds: [ google.maps.MapTypeId.ROADMAP, "tehgrayz" ]
-			 },
-			 scrollwheel: false
-		 });
-		 var e = new google.maps.StyledMapType([ {
-			 featureType: "all",
-			 stylers: [ {
-				 hue: "#0000ff"
-			 }, {
-				 saturation: -95
-			 } ]
-		 }, {
-			 featureType: "poi",
-			 elementType: "label",
-			 stylers: [ {
-				 visibility: "off"
-			 } ]
-		 } ], {
-			 name: "Grayscale"
-		 });
-		 window.Gmap.mapTypes.set("tehgrayz", e);
-		 window.Gmap.setMapTypeId("tehgrayz");
-		 new google.maps.Marker({
-			 position: new google.maps.LatLng(parseFloat(t[0]), parseFloat(t[1])),
-			 map: window.Gmap,
-			 clickable: false,
-			 icon: {
-				 url: a,
-				 origin: new google.maps.Point(0, 0)
-			 }
-		 });
-		 new google.maps.Marker({
-			 position: new google.maps.LatLng(parseFloat(55.7156), parseFloat(37.68178)),
-			 map: window.Gmap,
-			 clickable: false,
-			 icon: {
-				 url: "images/map-arrow.png",
-				 origin: new google.maps.Point(0, 0)
-			 }
-		 });
-		 new google.maps.Marker({
-			 position: new google.maps.LatLng(parseFloat(55.717678), parseFloat(37.675938)),
-			 map: window.Gmap,
-			 clickable: false,
-			 icon: {
-				 url: "images/metro.png",
-				 origin: new google.maps.Point(0, 0)
-			 }
-		 });
-		 var r = [ {
-			 lat: 55.717894,
-			 lng: 37.676866
-		 }, {
-			 lat: 55.714848,
-			 lng: 37.679173
-		 }, {
-			 lat: 55.715585,
-			 lng: 37.681705
-		 }, {
-			 lat: 55.7156,
-			 lng: 37.681782
-		 } ];
-		 var l = new google.maps.Polyline({
-			 path: r,
-			 geodesic: true,
-			 strokeColor: "#f4b816",
-			 strokeOpacity: 1,
-			 strokeWeight: 5
-		 });
-		 l.setMap(window.Gmap);
-	 };
- };
+	$("body").on("click", ".js-closed", function (e) {
+		e.preventDefault();
+		$(this).parent().parent().detach();
+		$('.hide-drop').show();
+	});
+
+
+})();
+
+var vacancy = (function(){
+	var link = $('.js-vac-link');
+
+	link.click(function(e){
+		e.preventDefault();
+		$(this).next().slideToggle();
+	});
+
+
+})();
+
+(function(window, document) {
+		'use strict';
+		var file = 'images/sprite.svg'; // путь к файлу спрайта на сервере
+
+		if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) return true;
+		var isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
+				request,
+				data,
+				insertIT = function() {
+						document.body.insertAdjacentHTML('afterbegin', data);
+				},
+				insert = function() {
+						if (document.body) insertIT();
+						else document.addEventListener('DOMContentLoaded', insertIT);
+				};
+		if (isLocalStorage && localStorage.getItem('inlineSVGrev') == revision) {
+				data = localStorage.getItem('inlineSVGdata');
+				if (data) {
+						insert();
+						return true;
+				}
+		}
+		try {
+				request = new XMLHttpRequest();
+				request.open('GET', file, true);
+				request.onload = function() {
+						if (request.status >= 200 && request.status < 400) {
+								data = request.responseText;
+								insert();
+								if (isLocalStorage) {
+										localStorage.setItem('inlineSVGdata', data);
+										localStorage.setItem('inlineSVGrev', revision);
+								}
+						}
+				}
+				request.send();
+		} catch (e) {}
+}(window, document));
